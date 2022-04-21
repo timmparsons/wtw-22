f301df1c87a91590430fd4c610ede5cc2222783e
Tim Parsons <timmparsons85@gmail.com>
2022-04-20 22:05:45 -0700

    fixed some config files

--- components/MovieSearchbox.tsx
+++ components/MovieSearchbox.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { MOVIE_DB_API } from '@env';
 
 interface Movie {
   s: string;
@@ -15,9 +16,9 @@ interface MovieTitle {
 
 export default function MovieSearchbox() {
   const API_URL = 'https://api.themoviedb.org/3/movie/popular';
-  const API_KEY = 'd5826b4e12c757147537031e74238c63';
   const PARAMS = 'page=1';
-  const BASE_URL = `${API_URL}?api_key=${API_KEY}&${PARAMS}`;
+  const apiKey = MOVIE_DB_API;
+  const BASE_URL = `${API_URL}?api_key=${apiKey}&${PARAMS}`;
 
   const [ movie, setMovie ] = useState<Movie>({
     s: 'Enter a movie..',
